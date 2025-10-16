85b17476dcba30faaf64865ab1d184616859c6dd
KaleniG <151833811+KaleniG@users.noreply.github.com>
2025-10-16 11:43:48 +0200

    Fixed sort

--- web/js/common/Sort.js
+++ web/js/common/Sort.js
@@ -1,26 +1,33 @@
 $(document).ready(function () {
   $('th.sortable').click(function () {
+    if (typeof this.asc === 'undefined') this.asc = true; // default ascending
+
     var table = $(this).parents('table').eq(0);
     var rows = table.find('tbody tr').toArray().sort(comparer($(this).index()));
-    this.asc = !this.asc
-    if (!this.asc) { rows = rows.reverse() }
-    for (var i = 0; i < rows.length; i++) { table.append(rows[i]) }
+
+    if (!this.asc) rows = rows.reverse(); // reverse if descending
+    for (var i = 0; i < rows.length; i++) table.append(rows[i]);
 
     const th = $(this);
     $('th.sortable').each(function () {
       $(this).text($(this).text().replace(/[▲▼]/g, ''));
     });
-    th.text(th.text() + (this.asc ? ' ▲' : ' ▼'));
-  })
+    th.text(th.text() + (this.asc ? ' ▼' : ' ▲'));
+
+    this.asc = !this.asc; // flip for next click
+  });
+
 
   function comparer(index) {
     return function (a, b) {
-      var valA = getCellValue(a, index), valB = getCellValue(b, index)
-      return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+      var valA = getCellValue(a, index), valB = getCellValue(b, index);
+      return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB);
     }
   }
 
   function getCellValue(row, index) {
-    return $(row).children('td').eq(index).text()
+    var cell = $(row).children('td').eq(index);
+    var input = cell.find('input, textarea');
+    return input.length ? input.val() : cell.text();
   }
 });
